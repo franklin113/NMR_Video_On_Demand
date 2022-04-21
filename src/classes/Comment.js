243b214a05d1fe54ab9056cb4979fe5d77588126
Tim Franklin <timsfranklin1@gmail.com>
2022-04-20 23:58:09 -0400

    removed the eventID from the Comment class

--- src/classes/Comment.js
+++ src/classes/Comment.js
@@ -1,13 +1,13 @@
 class Comment {
-  constructor({ attendeeId, email, eventId, firstName, lastName, text, timestamp, videoId }) {
+  constructor({ attendeeId, email, libraryId, firstName, lastName, text, timestamp, videoId }) {
     if (
-      !(attendeeId && email && eventId && firstName && lastName && text && timestamp && videoId)
+      !(attendeeId && email && libraryId && firstName && lastName && text && timestamp && videoId)
     ) {
       throw new Error('validation error while creating comment')
     }
     this.attendeeId = attendeeId
     this.email = email
-    this.eventId = eventId
+    this.libraryId = libraryId
     this.firstName = firstName
     this.lastName = lastName
     this.text = text
@@ -18,7 +18,7 @@ class Comment {
     return {
       attendeeId: this.attendeeId,
       email: this.email,
-      eventId: this.eventId,
+      libraryId: this.libraryId,
       firstName: this.firstName,
       lastName: this.lastName,
       text: this.text,
