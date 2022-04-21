776752929625bfdce0b7f36a290cd2b7a6f67593
Tim Franklin <timsfranklin1@gmail.com>
2022-04-21 12:58:14 -0400

    added a single comment component

--- src/components/comment_section/CommentList.vue
+++ src/components/comment_section/CommentList.vue
@@ -1,43 +1,31 @@
 <template>
   <div id="comments-list">
-    <div v-for="(comment, index) in comments" :key="index" class="single-comment">
-      <div class="left-section">
-        <div class="user-initials">TF</div>
-      </div>
-      <div class="mid-section">
-        <div class="comment-header">
-          <div class="comment-author">
-            {{ comment.firstName + ' ' + comment.lastName }}
-          </div>
-          <div class="comment-timestamp">
-            {{ convertTimestamp(comment.timestamp) }}
-          </div>
-        </div>
-        <div class="comment-body">
-          {{ comment.text }}
-        </div>
-      </div>
-    </div>
+    <SingleComment
+      v-for="(comment, index) in comments"
+      :key="index"
+      :comment="comment"
+    ></SingleComment>
   </div>
 </template>
 
 <script>
+import SingleComment from '@/components/comment_section/SingleComment'
 export default {
+  components: {
+    SingleComment,
+  },
   props: {
     comments: {
       type: Array,
       required: true,
     },
   },
-  methods: {
-    convertTimestamp(val) {
-      if (!val) return ''
-      return val.toDate().toLocaleString()
-    },
-  },
 }
 </script>
 
 <style scoped>
-
+div#comments-list {
+  display: flex;
+  flex-direction: column;
+}
 </style>
---
+++ src/components/comment_section/SingleComment.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="single-comment">
+    <div class="left-section">
+      <div class="user-initials">TF</div>
+    </div>
+    <div class="mid-section">
+      <div class="comment-header">
+        <div class="comment-author">
+          {{ comment.firstName + ' ' + comment.lastName }}
+        </div>
+        <div class="comment-timestamp">
+          {{ convertTimestamp(comment.timestamp) }}
+        </div>
+      </div>
+      <div class="comment-body">
+        {{ comment.text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  methods: {
+    convertTimestamp(val) {
+      if (!val) return ''
+      return val.toDate().toLocaleString()
+    },
+  },
+}
+</script>
+
+<style scoped>
+.single-comment {
+  display: flex;
+  flex: 1 0 100%;
+}
+
+.comment-header {
+  display: flex;
+}
+</style>
