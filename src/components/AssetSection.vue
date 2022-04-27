755f3a18e1c8fec875fda84ca13df0ff0a37d247
Tim Franklin <timsfranklin1@gmail.com>
2022-04-27 14:27:02 -0700

    added assets and speakers section

---
+++ src/components/AssetSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="assets-component">
+    <a v-for="(item, index) in assetsComputed" :key="index" class="single-asset">
+      <div class="single-asset-div">{{ item.title || 'Asset ' + index.toString() }}</div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    assets: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    assetsComputed: function () {
+      const copied = [...Object.values(this.assets)]
+      copied.sort((a, b) => (a.title < b.title ? -1 : 1))
+      copied.sort((a, b) => (a.idx || 0) - (b.idx || 0))
+
+      return copied
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SpeakerSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="speakers-component">
+    <a v-for="(item, index) in speakersComputed" :key="index" class="single-speaker">
+      <div class="single-speaker-div">{{ item.username || '' }}</div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    speakers: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    speakersComputed: function () {
+      const copied = [...Object.values(this.assets)]
+      copied.sort((a, b) => (a.username < b.username ? -1 : 1))
+      copied.sort((a, b) => (a.idx || 0) - (b.idx || 0))
+
+      return copied
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
