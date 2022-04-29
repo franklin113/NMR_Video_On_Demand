e9d032f4d5399f44dd50d9a5a80be7d8cbc267bd
Tim Franklin <timsfranklin1@gmail.com>
2022-04-28 17:03:56 -0700

    added test for the assetssection

---
+++ tests/unit/AssetsSection.spec.js
@@ -0,0 +1,47 @@
+import { shallowMount } from '@vue/test-utils'
+import AssetsSection from '@/components/AssetSection'
+
+describe('assets section', () => {
+  test('it does not crash', () => {
+    const wrapper = shallowMount(AssetsSection, {
+      propsData: {
+        assets: {
+          item1: {
+            title: 'test1',
+            url: 'https://www.google.com',
+          },
+          item2: {
+            title: 'test2',
+            url: 'https://www.google.com',
+          },
+        },
+      },
+    })
+    const items = wrapper.findAll('.single-asset-div')
+    expect(items.length).toBe(2)
+    expect(items.at(0).text()).toBe('test1')
+    expect(items.at(1).text()).toBe('test2')
+  })
+  test('it does not crash', () => {
+    const wrapper = shallowMount(AssetsSection, {
+      propsData: {
+        assets: {
+          item1: {
+            title: 'test1',
+            url: 'https://www.google.com',
+            idx: 1,
+          },
+          item2: {
+            title: 'test2',
+            url: 'https://www.google.com',
+            idx: 0,
+          },
+        },
+      },
+    })
+    const items = wrapper.findAll('.single-asset-div')
+    expect(items.length).toBe(2)
+    expect(items.at(0).text()).toBe('test2')
+    expect(items.at(1).text()).toBe('test1')
+  })
+})
